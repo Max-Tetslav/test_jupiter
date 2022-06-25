692c03b5f7060dc3a4dc76ec8d148a14106beaf8
Max Teclav <maks.tetslav@yandex.ru>
2022-06-25 16:46:44 +0300

    deploy: v0.1

--- src/components/common/spin/Spin.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import cl from './Spin.module.scss';
-
-const Spin = () => {
-  return <div className={cl.spin} />;
-};
-
-export default Spin;
--- src/components/common/wrapper/Wrapper.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import mergeCl from 'classnames';
-import cl from './Wrapper.module.scss';
-
-const Wrapper = ({ className, children }) => {
-  return <div className={mergeCl(cl.container, className)}>{children}</div>;
-};
-
-export default Wrapper;
